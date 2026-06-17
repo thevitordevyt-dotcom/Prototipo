3c89104d834099513ea8379c25a7c62de7b86b13
thevitordevyt@gmail.com <thevitordevyt@gmail.com>
2026-06-17 11:32:21 -0300

    Finalizado ( mais ou menos)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-const dark = document.querySelector(".dark");
+const botao = document.getElementById("dark")
 
-if (dark) {
-    dark.addEventListener("click", (event) => {
-        event.preventDefault();
-        document.body.classList.toggle("dark-mode");
-
-        if (document.body.classList.contains("dark-mode")) {
-            dark.textContent = "Modo Light";
-        } else {
-            dark.textContent = "Modo Dark";
-        }
-    });
-}
+botao.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")) {
+        botao.textContent = "☀️ Modo Light"; 
+    } else {
+        botao.textContent = "🌙 Modo Dark"; 
+    }
+});
